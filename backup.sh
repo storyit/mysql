2135577c15bbb8631adcbfdf6ba6cce61c710ca2
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2019-08-17 11:57:14 -0400

    removed flag

--- backup.sh
+++ backup.sh
@@ -2,4 +2,4 @@
 
 . db.config
 fn=`date -d "today" +"%Y_%m_%d_%H:%M"`
-mysqldump --column-statistics=0 -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD main > /var/mysql_backups/$fn.sql
+mysqldump -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD main > /var/mysql_backups/$fn.sql
